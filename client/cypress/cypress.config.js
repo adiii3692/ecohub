6548e167a18dcbcd0e3280a003708cb31d25e930
adiii3692 <adinair0206@gmail.com>
2025-02-19 14:50:07 -0500

    Disabled support file

--- client/cypress/cypress.config.js
+++ client/cypress/cypress.config.js
@@ -2,6 +2,6 @@ module.exports = {
   e2e: {
     baseUrl: 'http://localhost:5173/',
     specPattern: 'cypress/e2e/**/*.{js,ts}',
-    supportFile: 'cypress/support/e2e.js'
+    supportFile: false
   },
 };
